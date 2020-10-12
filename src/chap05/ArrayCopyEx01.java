1da4004c9c59b24b41b39c11f7693c335b3c4d06
haeinmin <haeinmin29@gmail.com>
2020-10-12 12:25:01 +0900

    copy

---
+++ src/chap05/ArrayCopyEx01.java
@@ -0,0 +1,22 @@
+package chap05;
+
+public class ArrayCopyEx01 {
+	public static void main(String[] args) {
+		int[] arr1 = {5, 6, 7};
+		
+		int[] arr2 = new int[arr1.length]; //참조값은 다름
+		
+//		arr2[0] = arr1[0];
+//		arr2[1] = arr1[1];
+//		arr2[2] = arr1[2];
+		
+		for (int i=0; i<arr1.length; i++) {
+			arr2[i] = arr1[i];
+		}
+		
+		for (int n : arr2) {
+			System.out.println(n);
+		}
+	}
+
+}
