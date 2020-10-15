78b2e551f7694102f3e541a27873da09aed11665
haeinmin <haeinmin29@gmail.com>
2020-10-15 09:30:29 +0900

    inheritence

---
+++ src/chap07/lecture/SuperClass.java
@@ -0,0 +1,10 @@
+package chap07.lecture;
+
+public class SuperClass {
+	int var1;
+	
+	void method1() {
+		System.out.println("super method");
+	}
+
+}
--- src/codingbat/fix23.java
+++ src/codingbat/fix23.java
@@ -1,5 +1,19 @@
 package codingbat;
 
 public class fix23 {
+		
+		public int[] fix23(int[] nums) {
+			  
+			  int[] fix23 = new int[3];
+			  
+			  if(nums[0]==2 && nums[1]==3) {
+			    return fix23 = {2, 0, nums[2]};
+			  } else if(nums[1]==2 && nums[2]==3) {
+			    return fix23 = {nums[0], 2, 0};
+			  } else {
+			    return nums;
+			  }
+			}
 
-}
+	}
+
