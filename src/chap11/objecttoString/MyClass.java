0431fa4b7a5ba920f3fb930ea57a0af79c0a794b
haeinmin <haeinmin29@gmail.com>
2020-10-21 11:56:58 +0900

    tostring

---
+++ src/chap11/objecttoString/MyClass.java
@@ -0,0 +1,22 @@
+package chap11.objecttoString;
+
+public class MyClass extends Object { //있든 없든 무조건 extends Object 왜냐하면 모든 클래슨느 object 하위
+	
+	private int age;
+	private String name;
+	private String password;
+	
+	public void setFields(int age, String name, String password) {
+		this.age = age;
+		this.name = name;
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "MyClass [age=" + age + ", name=" + name + ", password=" + password + "]";
+	}
+	
+	
+
+}
---
+++ src/chap11/objecttoString/ToStringEx1.java
@@ -0,0 +1,17 @@
+package chap11.objecttoString;
+
+public class ToStringEx1 {
+	public static void main(String[] args) {
+		MyClass o1 = new MyClass();
+		String s = o1.toString();
+		System.out.println(s); //chap11.objecttoString.MyClass@15db9742
+		
+		MyClass o2 = new MyClass();
+		String s2 = o2.toString();
+		System.out.println(s2);
+		
+		System.out.println(s2.toString());
+		System.out.println(s2); //상동
+	}
+	
+}
